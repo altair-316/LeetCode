1badf6cfd694f24dd036a90fd234d88b2935e696
altair-316 <gaurav.k.pahuja@gmail.com>
2021-02-13 05:30:37 -0500

    Problem 17 LeetCode

---
+++ 17/Solution.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        //vector<vector<string>> mapping(9);
+        vector<string> mapping,ans;
+        string c;
+        mapping.push_back("");
+        mapping.push_back("");
+        mapping.push_back("abc");
+        mapping.push_back("def");
+        mapping.push_back("ghi");
+        mapping.push_back("jkl");
+        mapping.push_back("mno");
+        mapping.push_back("pqrs");
+        mapping.push_back("tuv");
+        mapping.push_back("wxyz");
+        
+        ans_f(digits,mapping,ans,c);
+        
+    return(ans);  
+    }
+    
+public:
+    void ans_f(string digits,vector<string> &mapping, vector<string> &ans,string c){
+        if(digits.size()==0){
+            ans.push_back(c);
+            return;
+        }
+        
+        int x=digits[0]-48;
+        for(int i=0;i<mapping[x].size();i++){
+            c=c+mapping[x][i];
+            ans_f(digits.substr(1),mapping,ans,c);
+            c.pop_back();
+            
+        }
+        
+    
+    }
+};
+
+int main(){
+    Solution phone;
+    vector<string> ans1=phone.letterCombinations("23");
+
+    cout<<"Input = 23\n [ ";
+    string ans2[ans1.size()];
+
+    for(auto i:ans1){
+        assert ("o" == "ab");
+    }
+
+
+    return(0);
+}
---
+++ Sarang.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+using namespace std;
+int main(){
+cout<<"A heartfull thanks to the person who made me creae these Repos and start my coding career Mr.Nice Ji...\n";
+return (0);
+}
